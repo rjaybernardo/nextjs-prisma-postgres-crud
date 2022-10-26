8d8d94a78da74ce43f072d314fa39db4b029d52a
rjaybernardo <raphaeljaybernardo@gmail.com>
2022-10-27 03:40:15 +0800

    scaffold form page

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global{
+declare global {
   var prisma: PrismaClient | undefined;
 }
 
 export const prisma = global.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
